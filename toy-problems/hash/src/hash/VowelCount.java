16869ee28ac86362829473a3ab814349ee22f52a
CKiilu <cndeti@gmail.com>
2016-02-02 09:13:35 +0300

    Added hashmap

---
+++ toy-problems/hash/src/hash/VowelCount.java
@@ -0,0 +1,37 @@
+package hash;
+
+//VOWEL COUNT
+
+//Using JAVA, create a VowelCount class with a vowelCount method and a main method.
+//The vowelCount method should contain the algorithm and the main method
+//should be used for testing your algorithm.To make testing easier, make the vowelCount method static.
+
+//Given an input of a String sentence, count the number of vowels that occur in the sentence.
+//Return a HashMap object containing the vowels as keys and their counts as values.
+
+//Example:
+//HashMap vowelMap = VowelCount.vowelCount("mary had a little lamb");
+//vowelMap.get('a'); // 4
+//vowelMap.get('i'); // 1
+//vowelMap.get('e'); // 1
+//vowelMap.get('o'); // null
+//HashMap vowelMap = VowelCount.vowelCount("do we control our computers, or do they control us?");
+//vowelMap.get('o'); // 8
+//vowelMap.get('i'); // 3
+//vowelMap.get('e'); // 3
+//vowelMap.get('u'); // 3
+import java.util.*;
+
+public class VowelCount {
+ public VowelCount(){
+    HashMap<String, Integer> vowels = new HashMap<String, Integer>();
+    vowels.put("a", 0);
+    vowels.put("b", 1);
+    vowels.put("c", 2);
+    vowels.put("d", 3);
+    vowels.put("e", 4);
+ }
+public static void main(String[] args) {
+  HashMap vowels = vowels.VowelCount("Do you like to travel around the world?");
+}
+}
